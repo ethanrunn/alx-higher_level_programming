b48531e400fa3527769a980c2bdf5f1f74df2ba4
ethanrunn <ethanrunn5@gmail.com>
2022-12-20 06:07:34 -0800

    first commit

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -7,5 +7,5 @@ def safe_print_list_integers(my_list=[], x=0):
             real_num += 1
         except (ValueError, TypeError):
             continue
-        print()
-        return real_num
+    print()
+    return real_num
