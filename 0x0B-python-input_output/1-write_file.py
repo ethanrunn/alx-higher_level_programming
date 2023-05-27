37798e4d10197e4a665cc9ba25eeec117ae9bf87
Ethan Runn <81266738+ethanrunn@users.noreply.github.com>
2023-05-27 20:38:11 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/1-write_file.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""defining the function write_file"""
-
-
-def write_file(filename="", text=""):
-    """reads filename with utf-8"""
-    with open(filename, "w", encoding='utf-8') as my_file:
-        return my_file.write(text)
--- 0x0B-python-input_output/README.md
+++
@@ -1,1 +0,0 @@
-This directory contains files for the ALX 0x0B-python-input_output project
